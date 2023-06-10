592d97d9b93d38552554201122d5d520a6c19d14
shiro1309 <nicolay5lie@gmail.com>
2023-06-10 15:26:23 +0200

    modified:   download_v_4.py

--- download_v_4.py
+++ download_v_4.py
@@ -17,12 +17,45 @@ class download:
         time.sleep(0.5)
         pgi.keyUp("ctrl")
         
-    def download_click(self):
+    def page_nav(self):
         pgi.moveTo(1680, 280)
         pgi.leftClick()
+    
+    def page_down(self):
         time.sleep(3)
         pgi.moveTo(1680, 670)
         pgi.leftClick()
+        
+    def clickpage(self):
+        pgi.moveTo(100, 700)
+        pgi.leftClick()
+        
+    def goback(self):
+        pgi.keyDown("ctrl")
+        pgi.keyDown("shift")
+        pgi.press("tab")
+        time.sleep(0.1)
+        pgi.keyUp("ctrl")
+        pgi.keyUp("shift")
+        
+    def gofront(self):
+        pgi.keyDown("ctrl")
+        pgi.press("tab")
+        time.sleep(0.1)
+        pgi.keyUp("ctrl")
+    
+    def open_tabs(self, links):
+        for link in links:
+            wb.open(link)
+            time.sleep(0.2)
+            
+    def downprep(self,links):
+        self.open_tabs(links)
+        self.clickpage()
+        for i in range(len(links)-1):
+            self.page_nav()
+            self.goback()
+        self.page_nav()
 
 class get_meta_data:
     def get_all_data(self, series_data):
@@ -101,69 +134,31 @@ class App:
         print(self.series, self.seasons, self.episode_count)
         print("it will take " + str(round(sum(self.episode_count)*220/60/60/5, 2)) + " hours with 5Mb/s")
         print("it will be " + str(round(sum(self.episode_count)*220/1000, 2)) + " Gb of data if the episode average is 220MB")
-        #n = 0
+        n = 0
         while True:
-            list_2 = self.link_data[n*10:(n+1)*10]
-            list_3 = []
+            list_2 = self.url_list[n*10:(n+1)*10]
             
-            list_3.clear()
-            while True:
+            self.down.downprep(list_2) # opens and prepere the pages for download just need to wait for 32 seconds
+            time.sleep(32) # the waiting time
+            for i in range(0, len(list_2)): # loops over links but as a number
+                self.down.page_down() # starts the download
                 time.sleep(1)
                 
-                for episode in range(len(list_2)):
-                    file_list_old = 0
-                    file_list_new = 0
-                    
-                    if episode not in list_3:
+                # check if its downloading
+                #files = os.listdir("downloads")
+                #for file in files:
+                #    if ".part" not in file:
                         
-                        files = os.listdir("downloads")
-                        for file in files:
-                            if ".part" not in file:
-                                file_list_old += 1
-                                
-                        wb.open(list_2[episode])
-                        time.sleep(5)
-                        self.down.download_click()
-                        time.sleep(2)
-                        
-                        files = os.listdir("downloads")
-                        for file in files:
-                            if ".part" not in file:
-                                file_list_new += 1
-                                
-                        if file_list_new == file_list_old:
-                            val = False
-                        elif file_list_new > file_list_old:
-                            val = True
-                        
-                        if val == False:
-                            self.down.page_close()
-                            self.down.page_close()
-                        
-                        if val == True:
-                            self.down.page_close()
-                            list_3.append(episode)
-                            
-                            dir_files = os.listdir("downloads")
-                            
-                            for file in dir_files:
-                                if ".part" not in file:
-                                    if file not in self.old_name_list:
-                                        self.old_name_list.append(file)
-                                        self.new_name_list = self.file_name.note_name(self.series, self.seasons, self.episode_count, n*10+episode+1, self.new_name_list)
-                        print(episode)
-                if len(list_2) == len(list_3):
-                    break
-            
-            n += 1
+                
+                
+                self.down.gofront() # page shift
             
             files = os.listdir("downloads")
-            if any(".part" in s for s in files):
-                pass
-            else:
-                break
+            for file in files:
+                if ".part" not in file:    
+                    break
+            n += 1             
             
-        self.file_name.rename(self.old_name_list, self.new_name_list)
         
 if __name__ == '__main__':
     app = App()
